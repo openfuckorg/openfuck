e7662010dbd8093eccb4468a522e2f2c35420bef
Benjamin Riggs <deisum@gmail.com>
2016-12-09 23:04:11 -0600

    Building.

--- Server/openfuck/__init__.py
+++ Server/openfuck/__init__.py
@@ -1,3 +1,8 @@
+"""
+Library for running the OpenFUCK sex robot.
+"""
+
+from collections.abc import Sequence
 
 import attr
 
@@ -5,13 +10,24 @@ import attr
 __author__ = "riggs"
 
 
-def percentage_validator(instance, attriubute, value):
-    if not (0 <= value <= 1):
-        raise ValueError("Value must be between 0 and 1 (inclusive).")
+@attr.s
+class Stroke:
+    @staticmethod
+    def _percentage_validator(instance, attriubute, value):
+        if not (0 <= value <= 1):
+            raise ValueError("Value must be between 0 and 1 (inclusive).")
+
+    position = attr.ib(validator=_percentage_validator)
+    velocity = attr.ib(validator=_percentage_validator)  # May be some sort of curve in the future.
 
 
 @attr.s
-class Stroke:
-    position = attr.ib(validator=percentage_validator)
-    velocity = attr.ib(validator=percentage_validator)  # May be some sort of curve in the future.
+class Pattern:
+    @staticmethod
+    def _actions_validator(instance, attribute, value):
+        if not isinstance(value, Sequence) or \
+           not all([isinstance(obj, (Stroke, Pattern)) for obj in value]):
+            raise ValueError("Actions must be a sequence of Strokes or Patterns.")
 
+    repeat = attr.ib(validator=lambda x: x >= 0)
+    actions = attr.ib(validator=_actions_validator)
