0385357fcfe6eb2d00894ab4c48121c947d36f9c
Benjamin Riggs <deisum@gmail.com>
2016-12-12 13:18:11 -0600

    Screw protocols. Using coro-based API.

--- Server/openfuck/device.py
+++ Server/openfuck/device.py
@@ -3,63 +3,48 @@ Handle all hardware communication.
 """
 
 import asyncio
+import attr
 import logging
 import serial
 import serial.aio
 import sys
 
-from collections.abc import Sequence
-
 
 __author__ = "riggs"
 
 
-SWITCH = ('/dev/ttyACM0', 115200)
-VALVES = ('/dev/ttyACM1', 115200)
+SWITCH = {'port': '/dev/ttyACM0', 'baudrate': 115200}
+VALVES = {'port': '/dev/ttyACM1', 'baudrate': 115200}
 
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(name)s: %(message)s',
     stream=sys.stderr,
 )
-log = logging.getLogger('main')
-
-event_loop = asyncio.get_event_loop()
+log = logging.getLogger('device')
 
 
-class Serial(asyncio.Protocol):
-    def connection_made(self, transport):
-        self.transport = transport
-        self.log = logging.getLogger('Serial_{}'.format(self.transport))
-        self.log.debug('connected')
+@attr.s(frozen=True)
+class Serial:
+    reader = attr.ib()
+    writer = attr.ib()
 
-    def data_received(self, data):
-        self.log.debug('received {!r}'.format(data))
-        # TODO: Handle message.
-
-    def connection_lost(self, error):
-        if error:
-            self.log.error('ERROR: {}'.format(error))
-            event_loop.stop()
-        else:
-            self.log.debug('connection closed')
-        super().connection_lost(error)
-
-    def write(self, data):
-        if not isinstance(data, bytes):
-            if isinstance(data, Sequence):
-                data = bytes(data)
-            elif isinstance(data, int):
-                data = bytes((data,))
-            else:
-                raise TypeError("cannot convert to bytes: {}".format(data))
-        self.log.debug("writing {}".format(data))
-        self.transport.write(data)
+    @classmethod
+    async def connect(cls, loop, port, baudrate):
+        log.debug("connecting to {port} {baudrate}".format(port=port, baudrate=baudrate))
+        return cls(*await serial.aio.open_serial_connection(loop=loop, port=port, baudrate=baudrate))
 
 
-switch_coro = serial.aio.create_serial_connection(event_loop, Serial, *SWITCH)
-valves_coro = serial.aio.create_serial_connection(event_loop, Serial, *VALVES)
-switch_transport, switch_protocol = event_loop.run_until_complete(switch_coro)
-valves_transport, valves_protocol = event_loop.run_until_complete(valves_coro)
-log.debug("starting {} {}".format(*SWITCH))
-log.debug("starting {} {}".format(*VALVES))
+async def coro_factory(stroke, loop, stop_future, port, baudrate):
+    connection = await Serial.connect(loop=loop, port=port, baudrate=baudrate)
+    while not stop_future.done():
+        # TODO: Logic
+
+        pass
+    else:
+        connection.writer.close()
+
+
+async def connect(stroke, loop, stop_future):
+    loop.run_until_complete(coro_factory(stroke, loop, stop_future, **SWITCH))
+    loop.run_until_complete(coro_factory(stroke, loop, stop_future, **VALVES))
